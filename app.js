3f1aa72228e424cd13f23b611f353bb8b32524d5
Oluwafemi Makanjuola <68490063+FeMIMaKAY@users.noreply.github.com>
2022-10-12 10:59:22 +0100

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const myName = "Makanjuola Oluwafemi";
-const myHeight = "198 cm";
-const myCountry = "Nigeria";
-
-alert(`My name is ${myName}\nMy height is ${myHeight}\nMy country is ${myCountry}`)
